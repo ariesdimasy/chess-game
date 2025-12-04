aec33c842d07b8faac03c59a58bd770d5951651a
ariesdimasy <ariesdimasy@gmail.com>
2025-12-04 13:35:17 +0700

    update classes , adding pawn, add take chessman feature

--- index.js
+++ index.js
@@ -1,22 +1,82 @@
 import readline from "readline-sync";
-import boxen from "boxen"
-
 class Log {
     logs = []
-    addLog(piece, from, to ){
+    addLog(board, piece, from, to ){
+
+        let toConvert = convertCoordinate(to)
+        let take = ""
+        if(board[toConvert[0]][toConvert[1]] != "[ ]" && board[toConvert[0]][toConvert[1]] != "( )"){
+            take = board[toConvert[0]][toConvert[1]]
+            if( board[toConvert[0]][toConvert[1]] == piece){
+                take = ""
+            }
+        }
+
         this.logs.push({
-            piece, from, to
+            piece, from, to, take
         })
     }
 }
 
 class Piece { 
     name = ""
+    code=""
     direction = []
+    max
+    color=""
+    coor=""
+
+    constructor(name, code, color, coor){
+        this.name = name
+        this.code = code
+        this.color = color
+        this.coor = coor
+    }
+
+    move(){}
+    validation(){}
 }
 
 class Queen extends Piece {
+
+    direction=["forward","backward","left","right"]
+    max=false
     
+    constructor(color, coor){
+        super("queen", "Q"+color[0], color, coor)
+    }
+
+    validation(coor){
+
+    }
+}
+
+class Pawn extends Piece {
+    name="pawn"
+    direction=["forward"]
+    max=1
+    
+    constructor(color, coor){
+        super("pawn", "P"+color[0], color, coor)
+    }
+
+    validation(coor){
+
+    }
+}
+
+class King extends Piece {
+    name="king"
+    direction=["forward","backward","left","right"]
+    max=1
+
+    constructor(color, coor){
+        super("king", "K"+color[0], color, coor)
+    }
+
+    validation(coor){
+
+    }
 }
 
 function board(){
@@ -45,11 +105,22 @@ function board(){
 }
 
 function convertCoordinate(ccor){
+    let alpha = "ABCDEFGH"
+    //console.log("CCOR => ", ccor)
     let x = ccor[0]
     let y = Number(ccor[1]) 
-    let alpha = "ABCDEFGH"
+    if(alpha.split("").findIndex((item) => item == x) == -1){
+        console.log("invalid Coordinate")
+        return false
+    }
+    if(y < 1 && y > 8){
+        console.log("invalid Coordinate")
+        return false 
+    }
+   
     let findX = 8 - y
     let findY = alpha.split("").findIndex((item) => item == x)
+
     //console.log(ccor,findX, findY)
     return [findX, findY]
 }
@@ -70,19 +141,31 @@ function checkBoxColor(x,y){
     }
 }
 
-function move(board,logObj,turn,from,to){
+function move(board,logObj,from,to){
+    //console.log("move()", from, to)
     let fromConvert = convertCoordinate(from)
     let toConvert = convertCoordinate(to)
 
     //console.log("toConvert => ", to, toConvert)
-
+    
     let temp = board[fromConvert[0]][fromConvert[1]]
+    let toBoard = board[toConvert[0]][toConvert[1]]
+
+    if(temp == "[ ]" || temp == "( )" || !temp){
+        console.log("there is no Chessman in this location")
+        return false
+    }
+
+    if(!toBoard){
+        console.log("Invalid location")
+        return false
+    }
+
+    logObj.addLog(board,temp,from,to)
 
     board[fromConvert[0]][fromConvert[1]] = checkBoxColor(fromConvert[0], fromConvert[1])
     board[toConvert[0]][toConvert[1]] = temp
-
-    logObj.addLog(temp,from,to)
-    turn = (turn == "w") ? 'b' : 'w'
+    // turn = (turn == "w") ? 'b' : 'w'
 }
 
 function main(){
@@ -91,16 +174,18 @@ function main(){
     let win = false
     let turn = "w" // w hite || b lack
 
-    let whiteQueen = convertCoordinate("A1")
-    let whiteKing = convertCoordinate("A2")
+    let whiteQueen = new Queen("white",convertCoordinate("A1")) 
+    let whiteKing = new King("white",convertCoordinate("A2"))
 
-    let blackKing = convertCoordinate("B4")
+    let blackKing = new King("black",convertCoordinate("B4"))
+    let blackPawn = new Pawn("black",convertCoordinate("D4"))
 
-    mainBoard[whiteKing[0]][whiteKing[1]] = "Kw"
-    mainBoard[whiteQueen[0]][whiteQueen[1]] = "Qw"
-    mainBoard[blackKing[0]][blackKing[1]] = "Kb"
+    mainBoard[whiteKing.coor[0]][whiteKing.coor[1]] = whiteKing.code
+    mainBoard[whiteQueen.coor[0]][whiteQueen.coor[1]] = whiteQueen.code
+    mainBoard[blackKing.coor[0]][blackKing.coor[1]] = blackKing.code
+    mainBoard[blackPawn.coor[0]][blackPawn.coor[1]] = blackPawn.code
 
-    log1.addLog("Kw","A3","A2")
+    log1.addLog(mainBoard,"Kw","A3","A2")
 
     while(win == false){
         // console.clear()
@@ -112,7 +197,8 @@ function main(){
         console.log("turn : ", turn == "w" ? "white" : "black")
         let fromInput = readline.question("Init : ");
         let toInput = readline.question("Move to : ");
-        move(mainBoard, log1,turn, fromInput, toInput)
+        move(mainBoard, log1, fromInput, toInput)
+        console.log("-------------------------------------------------------------------------------------------------------------")
     }
 }
 
